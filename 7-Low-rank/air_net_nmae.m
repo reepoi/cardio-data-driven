4463e12ace545fe530c4ee6a0eb66f68fb1a463e
Taos Transue <spacechips@rocketmail.com>
2022-10-29 14:47:35 -0600

    fixing bug in matlab air-net nmae implementation

--- 7-Low-rank/air_net_nmae.m
+++ 7-Low-rank/air_net_nmae.m
@@ -9,7 +9,8 @@ function res = air_net_nmae(y, yhat, mask)
 % Returns
 % res - the NMAE as calculated in AIR-Net
 inv_mask = 1 - mask;
-abs_masked = abs((y - yhat) * inv_mask);
+ydiff = abs(y - yhat);
+ydiff_masked = ydiff(inv_mask == 1);
 vec_y = y(:);
-res = sum(abs_masked(:)) / sum(inv_mask(:)) / (max(vec_y) - min(vec_y));
+res = sum(ydiff_masked) / sum(inv_mask(:)) / (max(vec_y) - min(vec_y));
 end
